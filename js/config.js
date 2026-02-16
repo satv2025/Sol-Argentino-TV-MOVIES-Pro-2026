aa2c374eb05aed276bed847c5c9e196407cbbe54
satv2025 <traplegionrecords2020@gmail.com>
2026-02-15 21:56:11 -0300

    ACT

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // SATV config (public anon key is meant to be public)
 export const CONFIG = {
-  SUPABASE_URL: "https:///api.satvplus.com.ar",
+  SUPABASE_URL: "https://api.satvplus.com.ar",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrc2dhcWdha3p3dnFjdGVra2dqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5NTAwMzIsImV4cCI6MjA4NjUyNjAzMn0.dnJMB_Orqu_ldP7ODcs-VpZduaGPUEbe2u-yYJXk9Fc",
   APP_NAME: "SATV+",
   // Optional: restrict Upload page to admins only (email match)
